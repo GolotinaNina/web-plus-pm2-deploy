5c5610a8f08fba17cbcac1257d3cf285c9f255fa
MAX4020 <144027580+MAX4020@users.noreply.github.com>
2024-07-12 22:03:20 +0400

    change
    change

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
-// import cors from 'cors';
+import cors from 'cors';
 import errorHandler from './middlewares/error-handler';
 import { DB_ADDRESS } from './config';
 import routes from './routes';
@@ -15,9 +15,20 @@ mongoose.connect(DB_ADDRESS);
 
 // Только для локальных тестов. Не используйте это в продакшене
 // app.use(cors())
+
+app.use(cors({
+  origin: [
+    'http://0.0.0.0:3006',
+    'https://0.0.0.0:3006',
+    'https://mestogolotina.nomorepartiesco.ru',
+    'http://api.mestogolotina.nomorepartiesco.ru',
+  ],
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use(routes);
 app.use(errors());
 app.use(errorHandler);
--- backend/src/middlewares/error-handler.ts
+++ backend/src/middlewares/error-handler.ts
@@ -3,7 +3,7 @@ import { ErrorRequestHandler } from 'express';
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   const { statusCode = 500 } = err;
   const isErrorInternal = statusCode === 500;
-  const message = isErrorInternal ? 'На сервере произошла ошибка' : err.message;
+  const message = isErrorInternal ? 'На сервере произошла ошибка': err.message;
   res.status(statusCode).send({ message });
   next();
 };
